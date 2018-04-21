fc206d102a8c079dc03dcd6ef1560af07057986e
Philippe Hamel <p.hamel@protonmail.com>
2018-04-21 17:02:03 -0400

    Chapitre 1
    Structure du module d'importation

--- exercise/chap01/chap01.py
+++ exercise/chap01/chap01.py
@@ -1,6 +1,45 @@
 """
 Think Stats 2
-Exercise chapter 1
+Chapter 1 exercise 2
 Cleaning and importing module
 """
 
+from __future__ import print_function, division
+from collections import defaultdict
+
+import numpy as np
+import thinkstats2 as ts
+import sys
+
+def ReadFemResp(dct_file = '2002FemResp.dct',
+                dta_file = '2002FemResp.dta.gz',
+                nrows = None):
+    """
+    Read the NSFG data
+
+    :param dct_file: string
+    :param dta_file: string
+    :param nrows:
+    :return: dataframe
+    """
+    #TODO
+
+def CleanFemResp(df):
+    """
+    Clean the FemResp Data
+
+    :param df: dataframe
+    :return: dataframe
+    """
+    #TODO
+
+def main():
+    """
+    Test the fonction in the module
+    :return: nothing
+    """
+    #TODO
+
+
+if __name__ == "__main__":
+    main()
